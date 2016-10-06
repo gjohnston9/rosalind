7a120963cb700c48caef25b0b9dc5514e17aa7e8
Greg Johnston <greg.johnston@gatech.edu>
2016-10-06 18:56:17 -0400

    finished iev and splc, started using Biopython
    TODO: revise older solutions to use Biopython

---
+++ stronghold/iev.py
@@ -0,0 +1,24 @@
+# Calculating Expected Offspring
+
+from Bio import SeqIO
+
+with open("in.txt", "r") as infile, open("out.txt", "w") as outfile:
+	num_couples = infile.readline().strip("\n")
+	num_couples = map(float, num_couples.split())
+	# index | genotype of couple
+	#   0	|       AA-AA
+	#   1	|       AA-Aa
+	#   2	|       AA-aa
+	#   3	|       Aa-Aa
+	#   4	|       Aa-aa
+	#   5	|       aa-aa
+	avg_dominant_phenotype_offspring = [
+		2 * num_couples[0],
+		2 * num_couples[1],
+		2 * num_couples[2],
+		2 * num_couples[3] * 0.75,
+		2 * num_couples[4] * 0.5,
+		2 * num_couples[5] * 0,
+	]
+
+	outfile.write(str(sum(avg_dominant_phenotype_offspring)))
