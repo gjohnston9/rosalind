e146a064437acad1981e2b1d247e0b9a307cbb2f
greg9381 <greg.johnston@gatech.edu>
2017-05-10 17:26:41 -0400

    finish REVP

--- stronghold/revp.py
+++ stronghold/revp.py
@@ -2,17 +2,46 @@
 
 from Bio import SeqIO, Seq
 
-def palindromes(string, start, indices):
+from collections import defaultdict, namedtuple
+import pdb
 
+def is_reverse_complement(string, start, end):
+	fwd = string[start : end + 1]
+	return Seq.reverse_complement(fwd) == fwd
 
 with open("in.txt", "r") as infile, open("out.txt", "w") as outfile:
 	fasta_sequences = SeqIO.parse(infile, "fasta")
 	fasta = fasta_sequences.next()
-	fwd = str(fasta.seq)
-	rev = Seq.reverse_complement(fwd)
-	print fwd
-	# print rev
-	ind = {nuc : [index for index, char in enumerate(fwd) if char == nuc] for nuc in ("A", "T", "G", "C")}
-	palindromes = set()
-	print ind
+	seq = str(fasta.seq)
+	print seq
 
+	complement = {
+		"A" : "T",
+		"T" : "A",
+		"G" : "C",
+		"C" : "G"
+	}
+
+	matches = defaultdict(list)
+
+	rev_pal = namedtuple("Reverse_Palindrome", ["position", "length"])
+
+	### for each position in string, keep track of all potential ends of a reverse complement starting at this position 
+	for i in range(len(seq)):
+		for j in range(i + 1, len(seq), 2):
+			if seq[j] == complement[seq[i]]:
+				matches[i] = matches[i] + [j]
+
+	answers = set()
+
+	for start, ends in matches.iteritems():
+		longest = max(filter(lambda end: is_reverse_complement(seq, start, end), ends) or [None])
+		if longest != None:
+			length = longest - start + 1
+			if 4 <= length <= 12:
+				answers.add(rev_pal(position=start+1, length=length))
+
+	for index, t in enumerate(sorted(list(answers), key=lambda tup: tup.position)):
+		outfile.write("{} {}".format(t.position, t.length))
+		if index != len(answers) - 1:
+			outfile.write("\n")
